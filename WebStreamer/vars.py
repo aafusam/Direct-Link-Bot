dcc5f66cae7509f7a9dd08e4834ca451156aa26a
aafusam <62737721+aafusam@users.noreply.github.com>
2022-03-16 10:51:15 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -7,16 +7,16 @@ load_dotenv()
 
 
 class Var(object):
-    API_ID = int(getenv('8730742'))
-    API_HASH = str(getenv('383511e5138110f8e9a364d7e25d6451'))
-    BOT_TOKEN = str(getenv('5119665758:AAETCxcIzOBeJgLZOT42Haj209eEG49d8bQ'))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'AafuSam13'))
+    API_ID = int(getenv('16641929'))
+    API_HASH = str(getenv('af33474aed591a271d342070515e1fab'))
+    BOT_TOKEN = str(getenv('5077201853:AAEcwtNbeeEyjmhQ4SeYE3GFeLO-ltNHFiw'))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'AafuSam013'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('-1001322160987'))
+    BIN_CHANNEL = int(getenv('-1001606422438'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    OWNER_ID = int(getenv('5121304403'))
+    OWNER_ID = int(getenv('2075300611'))
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     if 'DYNO' in environ:
@@ -27,7 +27,7 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
         "http://{}:{}/".format(FQDN, PORT)
-    DATABASE_URL = str(getenv('DATABASE_URL'))
+    DATABASE_URL = str(getenv('mongodb+srv://afiqsam:afiqsam@cluster0.0wb1c.mongodb.net/afiqsam?retryWrites=true&w=majority'))
     PING_INTERVAL = int(getenv('PING_INTERVAL', '500'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001296894100")).split()))
+    UPDATES_CHANNEL = str(getenv('https://t.me/StarterNetworkz', None))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "BANNED_CHANNELS")).split()))
