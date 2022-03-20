c3e2910dbe5a084cd4dedaf8aa49bd21d00d537e
aafusam <62737721+aafusam@users.noreply.github.com>
2022-03-20 14:27:33 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -1,5 +1,3 @@
-# (c) @AvishkarPatil | @EverythingSuckz
-
 from os import getenv, environ
 from dotenv import load_dotenv
 
@@ -7,26 +5,27 @@ load_dotenv()
 
 
 class Var(object):
-    API_ID = str(getenv('16641929'))
-    API_HASH = str(getenv('af33474aed591a271d342070515e1fab'))
-    BOT_TOKEN = str(getenv('5077201853:AAEcwtNbeeEyjmhQ4SeYE3GFeLO-ltNHFiw'))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'AafuSam013'))
-    SLEEP_THRESHOLD = str(getenv('SLEEP_THRESHOLD', '60'))
-    WORKERS = str(getenv('WORKERS', '4'))
-    BIN_CHANNEL = str(getenv('-1001606422438'))
-    PORT = str(getenv('PORT', 8080))
+    API_ID = int(getenv('API_ID'))
+    API_HASH = str(getenv('API_HASH'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'AviStreamBot'))
+    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(getenv('WORKERS', '4'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    OWNER_ID = str(getenv('2075300611'))
+    OWNER_ID = int(getenv('OWNER_ID', '797848243'))
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('aafusam_directlinkkk'))
+        APP_NAME = str(getenv('APP_NAME'))
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
         "http://{}:{}/".format(FQDN, PORT)
-    DATABASE_URL = str(getenv('mongodb+srv://afiqsam:afiqsam@cluster0.0wb1c.mongodb.net/afiqsam?retryWrites=true&w=majority'))
+    DATABASE_URL = str(getenv('DATABASE_URL'))
     PING_INTERVAL = int(getenv('PING_INTERVAL', '500'))
-    
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001296894100")).split()))
